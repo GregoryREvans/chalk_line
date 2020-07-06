76c99ffdf93fccf9db453c25cfb812b39d886e68
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-06 10:54:38 -0400

    Cache.

---
+++ chalk_line/segments/segment_03/.persistent.py
@@ -0,0 +1,12 @@
+info = abjad.OrderedDict(
+    [
+        (
+            'Voice 1',
+            [
+                abjad.LilyPondLiteral('\\stopStaff \\once \\override Staff.StaffSymbol.line-count = #0 \\startStaff', format_slot='before', ),
+                abjad.LilyPondLiteral('\\once \\override Rest.color = #white', format_slot='before', ),
+                abjad.LilyPondLiteral('\\colophon', format_slot='absolute_after', ),
+                ],
+            ),
+        ]
+    )

e346612cee5898d85a06af9c6910ef248dba6001
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-06 14:21:36 -0400

    Cache.

---
+++ chalk_line/segments/segment_01/.handlers.py
@@ -0,0 +1,40 @@
+import abjad
+
+handler_to_value = abjad.OrderedDict(
+    [
+        ("tempo_handler_one", abjad.OrderedDict([("count", 3),]),),
+        (
+            "flute_grace_handler_one",
+            abjad.OrderedDict([("vector_count", 182), ("gesture_count", 43),]),
+        ),
+        ("chord_1", abjad.OrderedDict([("count", 265),]),),
+        ("glissando_handler_one", abjad.OrderedDict([("count", 265),]),),
+        ("trill_handler_one", abjad.OrderedDict([("count", 265),]),),
+        (
+            "notehead handler one",
+            abjad.OrderedDict(
+                [
+                    ("count", 265),
+                    ("head_vector_count", 265),
+                    ("transition_vector_count", 265),
+                ]
+            ),
+        ),
+        (
+            "dynamic_handler_one",
+            abjad.OrderedDict(
+                [
+                    ("count_1", 96),
+                    ("count_2", 46),
+                    ("count_3", 49),
+                    ("count_4", 49),
+                    ("count_5", 96),
+                ]
+            ),
+        ),
+        (
+            "articulation_handler_one",
+            abjad.OrderedDict([("count", 265), ("vector_count", 265),]),
+        ),
+    ]
+)
---
+++ chalk_line/segments/segment_03/.handlers.py
@@ -0,0 +1,30 @@
+import abjad
+
+handler_to_value = abjad.OrderedDict(
+    [
+        ("tempo_handler_one", abjad.OrderedDict([("count", 3),]),),
+        ("chord_1", abjad.OrderedDict([("count", 86),]),),
+        (
+            "notehead handler one",
+            abjad.OrderedDict(
+                [
+                    ("count", 86),
+                    ("head_vector_count", 86),
+                    ("transition_vector_count", 86),
+                ]
+            ),
+        ),
+        (
+            "dynamic_handler_one",
+            abjad.OrderedDict(
+                [
+                    ("count_1", 73),
+                    ("count_2", 25),
+                    ("count_3", 47),
+                    ("count_4", 47),
+                    ("count_5", 73),
+                ]
+            ),
+        ),
+    ]
+)
